3704f6b419d5ecd022334bfd5a2191d7d7555138
ZaidP101 <zpatel044@gmail.com>
2024-06-04 23:56:02 +0530

    OS.py added

---
+++ OS.py
@@ -0,0 +1,10 @@
+# 'python docs os' : This will help you to carry out multiple tasks using python in operating System
+
+import os
+import shutil
+print(os.getcwd())
+# current working Directory
+print(shutil.disk_usage("/"))
+# sh = shell (any work related to shell 'shutil' is used) "/" = root (entire system)
+# pip install xyz (any library) = pip is python package installer
+# pip install boto3 = boto3 is a library the allows you to acess your AWS using python
